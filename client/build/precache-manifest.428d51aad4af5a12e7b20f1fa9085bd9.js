614e19817d2db43c5269363776bb98c834013628
reidsteven75 <reidsteven75@gmail.com>
2019-01-12 19:42:04 -0700

    prod

---
+++ client/build/precache-manifest.428d51aad4af5a12e7b20f1fa9085bd9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3c26bce6071a1f4caf7c",
+    "url": "/static/js/main.3c26bce6.chunk.js"
+  },
+  {
+    "revision": "527f493adaea1fed03c2",
+    "url": "/static/js/1.527f493a.chunk.js"
+  },
+  {
+    "revision": "3c26bce6071a1f4caf7c",
+    "url": "/static/css/main.909e6f9d.chunk.css"
+  },
+  {
+    "revision": "03d352d6e7eb2285399526664c398a31",
+    "url": "/index.html"
+  }
+];
