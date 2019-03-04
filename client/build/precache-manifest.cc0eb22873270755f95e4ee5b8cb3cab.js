2c47894d072c10f09e9a851900ac78240dbd6226
reidsteven75 <reidsteven75@gmail.com>
2019-03-04 15:51:52 -0700

    prod build

---
+++ client/build/precache-manifest.cc0eb22873270755f95e4ee5b8cb3cab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "dfaf0653d564ad496638",
+    "url": "/static/js/main.dfaf0653.chunk.js"
+  },
+  {
+    "revision": "5bd46073d8d71dd868dc",
+    "url": "/static/js/1.5bd46073.chunk.js"
+  },
+  {
+    "revision": "dfaf0653d564ad496638",
+    "url": "/static/css/main.3eb532c4.chunk.css"
+  },
+  {
+    "revision": "5aa763a031f7b46c925a7a4f89db14e2",
+    "url": "/index.html"
+  }
+];
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8229565baa13e50284c281a93f977638.js"
+  "/precache-manifest.cc0eb22873270755f95e4ee5b8cb3cab.js"
 );
 
 workbox.clientsClaim();
